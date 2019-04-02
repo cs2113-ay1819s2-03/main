f3bc191e4a26b07539e18c8caa1f17adc791e446
yow ren jie <yow.ren.jie@gmail.com>
2019-04-02 23:17:25 +0800

    update test for compare and taskhistory

---
+++ src/test/java/seedu/project/logic/commands/TaskHistoryCommandTest.java
@@ -0,0 +1,105 @@
+package seedu.project.logic.commands;
+
+import static seedu.project.logic.commands.CommandTestUtil.*;
+import static seedu.project.model.Model.PREDICATE_SHOW_ALL_TASKS;
+import static seedu.project.testutil.TypicalIndexes.INDEX_FIRST_TASK;
+import static seedu.project.testutil.TypicalTasks.getTypicalProjectList;
+
+import org.junit.Test;
+
+import seedu.project.commons.core.Messages;
+import seedu.project.commons.core.index.Index;
+import seedu.project.logic.CommandHistory;
+import seedu.project.logic.LogicManager;
+import seedu.project.model.Model;
+import seedu.project.model.ModelManager;
+import seedu.project.model.ProjectList;
+import seedu.project.model.UserPrefs;
+import seedu.project.model.project.Project;
+import seedu.project.model.task.Task;
+import seedu.project.testutil.TaskBuilder;
+
+import java.util.ArrayList;
+import java.util.List;
+public class TaskHistoryCommandTest {
+    private Model model = new ModelManager(getTypicalProjectList(), new Project(), new UserPrefs());
+    private CommandHistory commandHistory = new CommandHistory();
+
+    @Test
+    public void execute_validIndexWithEdit() {
+
+        model.setProject(model.getFilteredProjectList().get(0));
+        model.setSelectedProject(model.getFilteredProjectList().get(0));
+
+        showTaskAtIndex(model, INDEX_FIRST_TASK);
+
+        String taskId = Integer.toString(model.getFilteredTaskList().get(0).getTaskId());
+
+        Task taskInFilteredList = model.getFilteredTaskList().get(INDEX_FIRST_TASK.getZeroBased());
+        Task editedTask1 = new TaskBuilder(taskInFilteredList).withName(VALID_NAME_CP2106).build();
+        Task editedTask2 = new TaskBuilder(taskInFilteredList).withName(VALID_NAME_CS2101).build();
+        TaskHistoryCommand taskHistoryCommand = new TaskHistoryCommand(INDEX_FIRST_TASK);
+
+        String tempCurrent1 = "edit 1 n/Orbital Project";
+        String tempCurrent2 = "edit 1 n/Milestone";
+        ArrayList<String> commandList = new ArrayList<>();
+        commandList.add(tempCurrent1);
+        commandList.add(tempCurrent2);
+        commandHistory.add("select 1");
+        commandHistory.addHistoryTaskId(taskId);
+        commandHistory.add(tempCurrent1);
+        commandHistory.addHistoryTaskId(taskId);
+        commandHistory.add(tempCurrent2);
+
+
+        String expectedMessage = String.format(TaskHistoryCommand.MESSAGE_SUCCESS, INDEX_FIRST_TASK.getOneBased(), commandList);
+
+        Model expectedModel = new ModelManager(
+                new ProjectList(model.getProjectList()), new Project(model.getProject()), new UserPrefs());
+
+        expectedModel.setProject(model.getFilteredProjectList().get(0));
+        expectedModel.setSelectedProject(model.getFilteredProjectList().get(0));
+        model.setProject(model.getFilteredProjectList().get(0));
+        model.setSelectedProject(model.getFilteredProjectList().get(0));
+
+        LogicManager.setState(true);
+
+        //Add first task
+        expectedModel.setTask(model.getFilteredTaskList().get(0), editedTask1);
+        expectedModel.updateFilteredTaskList(PREDICATE_SHOW_ALL_TASKS);
+        expectedModel.commitProject();
+        expectedModel.setProject(expectedModel.getSelectedProject(),
+                (Project) expectedModel.getProject()); //sync project list
+        expectedModel.commitProjectList();
+
+        //Add second task
+        expectedModel.setTask(expectedModel.getFilteredTaskList().get(0), editedTask2);
+        expectedModel.updateFilteredTaskList(PREDICATE_SHOW_ALL_TASKS);
+        expectedModel.commitProject();
+
+        expectedModel.setProject(expectedModel.getSelectedProject(),
+                (Project) expectedModel.getProject()); //sync project list
+        expectedModel.commitProjectList();
+
+        //Add first task
+        model.setTask(model.getFilteredTaskList().get(0), editedTask1);
+        model.updateFilteredTaskList(PREDICATE_SHOW_ALL_TASKS);
+        model.commitProject();
+        model.setProject(model.getSelectedProject(),
+                (Project) model.getProject()); //sync project list
+        model.commitProjectList();
+
+        //Add second task
+        model.setTask(model.getFilteredTaskList().get(0), editedTask2);
+        model.updateFilteredTaskList(PREDICATE_SHOW_ALL_TASKS);
+        model.commitProject();
+        model.setProject(model.getSelectedProject(),
+                (Project) model.getProject()); //sync project list
+        model.commitProjectList();
+
+        //String expectedMessage = String.format(compareCommand.MESSAGE_COMPARE_TASK_SUCCESS,
+        //tempString.get(0), tempString.get(1));
+
+        assertCommandSuccess(taskHistoryCommand, model, commandHistory, expectedMessage, expectedModel);
+    }
+}
